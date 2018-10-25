37315c98fc8b3b95f9b80aa68b15c1f357bb18d4
JurajMa <jm2111@cam.ac.uk>
2018-10-25 12:13:29 +0100

    Proper unittesting for CC4 added.

--- tests/test_table_table.py
+++ tests/test_table_table.py
@@ -16,12 +16,28 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
-#TODO Create test that isolates only CC4, everything up to it
+class TableCC4(Table):
+    """Derivative of Table used to isolate testing for CC4"""
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def label_sections(self):
+        """Labels only CC4"""
+        cc4 = self.find_cc4()
+        log.info("Table Cell CC4 = {}".format(cc4))
+        self.labels[cc4] = 'CC4'
+
 class TestCC4(unittest.TestCase):
+
+    def label_sections(self):
+        """Labels only CC4"""
+        cc4 = self.find_cc4()
+        log.info("Table Cell CC4 = {}".format(cc4))
+        self.labels[cc4] = 'CC4'
 
     def do_table(self, input_path, expected):
         log.debug("Test CC4, Table: {}".format(input_path))
-        table = Table(input_path)
+        table = TableCC4(input_path)
         result = table.find_cc4()
         log.debug("Result = {}".format(result))
         self.assertTupleEqual(expected, result)
@@ -32,8 +48,13 @@ class TestCC4(unittest.TestCase):
         self.do_table(input_path, expected)
 
     def test_cc4_2(self):
+        """
+        Here, the true result would actually be (4,5).
+        However, in find_cc4() we choose the criterion that the data region starts, from below, when at least half of
+        the row is non-empty.
+        """
         input_path = './data/table_example2.csv'
-        expected = (4,5)
+        expected = (3,5)
         self.do_table(input_path, expected)
 
     def test_cc4_3(self):
@@ -60,10 +81,6 @@ class TestCC4(unittest.TestCase):
         input_path = './data/table_example7.csv'
         expected = (8,10)
         self.do_table(input_path, expected)
-
-
-
-
 
 
 
